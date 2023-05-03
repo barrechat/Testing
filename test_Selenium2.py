756b1e5c7a65a3c7b3c2a48259195266bb492903
Aritz <aritz478@gmail.com>
2023-05-03 15:14:01 +0200

    Prueba argumentos

--- test_Selenium2.py
+++ test_Selenium2.py
@@ -14,6 +14,7 @@ def setup_module(self):
         options.add_argument('--window-size=1920,1080')
         options.add_argument("--headless")
         options.add_argument("--start-maximized")
+        options.add_argument('--no-sandbox')
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
         driver.implicitly_wait(30)
         driver.minimize_window()
